b8aca245288284b213d7362ee84e88f1805e3067
sammyd0e <saremu9@gmail.com>
2026-04-23 03:15:24 +0100

    gunicorn

--- backend/eccomerce/wsgi.py
+++ backend/eccomerce/wsgi.py
@@ -7,6 +7,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clinic.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eccomerce.settings')
 
 application = get_wsgi_application()
